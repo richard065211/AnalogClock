5c27ec953110db40aab10c6519261d6ae9d6513b
richard065211 <zhangzhenzhen1999@163.com>
2018-10-08 21:17:34 +0800

    student_manager

---
+++ analogclock/src/main/java/com/example/analogclock/AnalogClock.java
@@ -0,0 +1,13 @@
+package com.example.analogclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnalogClock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analog_clock);
+    }
+}
